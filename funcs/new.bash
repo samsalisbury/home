327fc33a03cd6f84ebad4c8e668d9ab4324da086
Sam Salisbury <samsalisbury@gmail.com>
2022-01-21 09:39:39 +0000

    new: open already-existing files

--- funcs/new.bash
+++ funcs/new.bash
@@ -3,9 +3,13 @@ new() {
 	(
 		set -eu
 		test -n "$1"
-		test -f "$1" && { echo "File '$1' already exists."; exit 1; }
-		printf '#!/usr/bin/env bash\n\nset -Eeuo pipefail\n\n' > "$1"
-		chmod +x "$1"
+		if test -f "$1"; then
+			echo "File '$1' already exists, opening as-is..."
+			sleep 1
+		else
+			printf '#!/usr/bin/env bash\n\nset -Eeuo pipefail\n\n' > "$1"
+			chmod +x "$1"
+		fi
 	) && {
 		if [[ "$EDITOR" = *vim ]]; then
 			"$EDITOR" +\$ "$1"
