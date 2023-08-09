6df671dbd85762c1c90ba72d2314a2dded07140b
Sam Salisbury <samsalisbury@gmail.com>
2023-08-09 17:57:11 +0000

    pkgs: devenv

--- .bashrc.d/15-packages.bash
+++ .bashrc.d/15-packages.bash
@@ -11,11 +11,40 @@ packages() {
 	has tailscale || msg "tailscale not installed; use 'install_tailscale'"
 }
 
-install_devenv() {
-	sudo nix-env -iA cachix -f https://cachix.org/api/v1/install
-	sudo cachix use devenv
-	sudo nix-env -if https://install.devenv.sh/latest
+APT_PACKAGES=(
+	coreutils moreutils build-essential net-tools
+	mosh 
+	git
+	golang-1.20-go
+)
 
+SNAP_PACKAGES=(
+	gh
+)
+
+CLASSIC_SNAP_PACKAGES=(
+	nvim
+)
+
+aptinstall() {
+	sudo env DEBIAN_FRONTEND=noninteractive apt install -y "$@"
+}
+
+install_apt() {
+	sudo apt update && aptinstall "${APT_PACKAGES[@]}"
+}
+
+install_snap() {
+	sudo snap install "${SNAP_PACKAGES[@]}"
+	sudo snap install --classic "${CLASSIC_SNAP_PACKAGES[@]}"
+}
+
+install_devenv() {
+	echo "trusted-users = root ubuntu" | \
+		sudo tee -a /etc/nix/nix.conf && sudo pkill nix-daemon
+	nix profile install nixpkgs#cachix
+	cachix use devenv
+	nix profile install --accept-flake-config tarball+https://install.devenv.sh/latest
 }
 
 install_tailscale() {
