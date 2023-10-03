0b3a564003fcc90ce0dfa99d6ac0ae77f525f241
Sam Salisbury <samsalisbury@gmail.com>
2023-10-03 10:15:31 +0100

    conf(nvim): upgrade and fix stuff

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,9 +1,10 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  config = function()
-    require("indent_blankline").setup({
-      show_first_indent_level = false,
-      show_trailing_blankline_indent = false,
-    })
-  end,
+  main = "ibl",
+  --config = function()
+  --  require("indent_blankline").setup({
+  --    show_first_indent_level = false,
+  --    show_trailing_blankline_indent = false,
+  --  })
+  --end,
 }
