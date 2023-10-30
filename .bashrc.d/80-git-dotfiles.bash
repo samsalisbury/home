de4d58a0fff8fe67e3128e9e3bc0a054a68fc0e8
Sam Salisbury <samsalisbury@gmail.com>
2023-10-30 10:57:08 +0000

    home: func to checkpoint lazy nvim plugins

--- .bashrc.d/80-git-dotfiles.bash
+++ .bashrc.d/80-git-dotfiles.bash
@@ -6,6 +6,13 @@ git-dotfiles() {
 	git_dotfiles_configure_shell_hook() {
 		# Override 'ga' func to add only modified files by default.
 		ga() { if [[ -z "$*" ]]; then git ls-files -m | xargs git add; else git add "$@"; fi; }
+		# Add func to checkpoint lazy.nvim plugins.
+		lazy() {
+			git reset
+			git add ~/.config/nvim/lazy-lock.json
+			git commit -m "conf(nvim): update plugins"
+			git push
+		}
 	}
 	git_dotfiles home "$HOME"
 	git_dotfiles system /
