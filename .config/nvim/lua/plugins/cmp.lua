2880786c8266590e96b971aae191bda030ee1300
Sam Salisbury <samsalisbury@gmail.com>
2024-04-04 16:19:45 +0100

    attempt to slow down completion suggestions

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = {
+      -- Slow down completion suggestions
+      performance = {
+        debounce = 250,
+        throttle = 1000,
+      },
+    },
+  },
+}
