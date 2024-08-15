cea1f3213c2461628d757b04743a3c87eaaf2f32
Sam Salisbury <samsalisbury@gmail.com>
2024-08-15 09:55:28 +0100

    nvim config tweaks

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -14,7 +14,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
-    { import = "lazyvim.plugins.extras.lang.python" },
+    -- { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.dap.core" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -6,14 +6,22 @@ return {
         flags = {
           debounce_text_changes = 500,
         },
+        servers = {
+          ltex = false,
+          ltex_ls = false,
+        },
+        inlay_hints = { enabled = false },
+        ltex = {
+          enabled = false,
+        },
       },
     },
   },
-  {
-    "tamago324/nlsp-settings.nvim",
+  --{
+  --  "tamago324/nlsp-settings.nvim",
 
-    requires = {
-      "neovim/nvim-lspconfig",
-    },
-  },
+  --  requires = {
+  --    "neovim/nvim-lspconfig",
+  --  },
+  --},
 }
