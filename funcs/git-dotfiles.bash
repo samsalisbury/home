9aa47263e39cf1bf59788d92bea0fd9b0ad639bf
Sam Salisbury <samsalisbury@gmail.com>
2024-11-29 10:39:37 +0000

    git-dotfiles: better gitignore config

--- funcs/git-dotfiles.bash
+++ funcs/git-dotfiles.bash
@@ -33,33 +33,73 @@ git_dotfiles() {
 	else
 		# Create the alias to launch a new shell for interacting with this git repo.
 		# shellcheck disable=SC2139 # We want the alias definition to be expanded right now.
-		alias "$ALIAS=_gdf_launch_shell '$WORKTREE' '$CUSTOM_DOT_GIT'"
+		alias "$ALIAS=_gdf_launch_shell '$NAME' '$WORKTREE'"
 	fi
 }
 
 _gdf_launch_shell() {
-	local WORKTREE="${1:?}"
-	CUSTOM_DOT_GIT="${2:?}"
+	local NAME="${1:?}" WORKTREE="${2:?}" CUSTOM_DOT_GIT
+	CUSTOM_DOT_GIT="$WORKTREE/$NAME.git"
 	[[ -d "$WORKTREE" ]] || return "$(_gdf_err "worktree '$WORKTREE' is not a directory")"
 	# If the worktree is not writable by the current user, we'll launch the shell with sudo...
 	local SUDO
 	[[ -w "$WORKTREE" ]] || SUDO=sudo
-	_gdf_init_repo "$WORKTREE" "$CUSTOM_DOT_GIT" || return 1
+	_gdf_init_repo "$NAME" "$WORKTREE" || return 1
 	$SUDO /usr/bin/env GIT_DIR="$CUSTOM_DOT_GIT" GIT_WORK_TREE="$WORKTREE" "${GDF_SHELL[@]}"
 }
 
 # _gdf_init_repo ensures the worktree is set up as a git repo. It uses subshell semantics
 # so it doesn't affect the current shell session.
 _gdf_init_repo() (
-	local WORKTREE="${1:?}" CUSTOM_DOT_GIT="${2:?}"
+	local NAME="${1:?}" WORKTREE="${2:?}" CUSTOM_DOT_GIT CUSTOM_DOT_GITIGNORE
+	CUSTOM_DOT_GIT="$WORKTREE/$NAME.git"
+	CUSTOM_DOT_GITIGNORE="$WORKTREE/$NAME.gitignore"
 	cd "$WORKTREE" || return "$(_gdf_err "failed to CD to worktree '$WORKTREE'")"
 	export GIT_DIR="$CUSTOM_DOT_GIT"
 	[[ -d "$CUSTOM_DOT_GIT" ]] || {
 		_gdf_log "first run for $NAME: $WORKTREE"
 		git init || return "$(_gdf_err "failed to initialize git repo")"
 	}
-	git config status.showUntrackedFiles no || return "$(_gdf_err "failed to configure git")"
-	git config core.excludesFile
+	[[ -e "$CUSTOM_DOT_GITIGNORE" ]] || {
+		_gdf_log "creating $CUSTOM_DOT_GITIGNORE"
+		cat >"$CUSTOM_DOT_GITIGNORE" <<-EOF
+			### git-dotfiles section (do not edit)
+			# Ignore everything except this file by default.
+			/*
+			!/home.gitignore
+			### End of git-dotfiles section.
+
+			# Common ignore patterns.
+			*.env
+			*.orig
+			*.bak
+			.DS_Store
+
+			# Files that commonly contain credentials.
+			auth.*
+			*.ign
+			*.json
+			*.yaml
+			*.yml
+
+			# Unignore some top-level directories, we will re-ignore some of their contents
+			# later. (This is needed because once a dir is excluded it can't be re-included.)
+			!/.config
+			!/.tmux
+			/.tmux/plugins
+			/.tmux/resurrect
+			!/.tmux.conf
+			!/.config/nvim
+			!/.bashrc.d
+
+			# Unignore some common dotfiles.
+			!/.bash_profile
+			!/.bashrc
+			!/Brewfile
+		EOF
+	}
+	git config status.showUntrackedFiles yes || return "$(_gdf_err "failed to configure git")"
+	git config core.excludesFile "$CUSTOM_DOT_GITIGNORE" || return "$(_gdf_err "failed to configure git")"
 )
 
 # _gdf_configure_shell is called inside the login shell launched by the alias.
