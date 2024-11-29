e814eab5c063f8c624387ec0f9f8b0a323b175f2
Sam Salisbury <samsalisbury@gmail.com>
2024-11-29 09:14:40 +0000

    formatting

--- funcs/git-dotfiles.bash
+++ funcs/git-dotfiles.bash
@@ -1,11 +1,13 @@
-
 _gdf_log() { echo "git_dotfiles: $*" 1>&2; }
-_gdf_err() { _gdf_log "error: $*"; echo 1; }
+_gdf_err() {
+	_gdf_log "error: $*"
+	echo 1
+}
 
 GDF_SHELL=(/bin/bash -l)
 
 # git_dotfiles is used for managing dotfiles directly using git.
-# 
+#
 # It creates an alias which launches a new bash shell configured to use
 # a custom dot git directory. Use of this custom dot git means that other
 # git repositories inside the same filesystem tree are conveniently ignored
@@ -17,24 +19,27 @@ GDF_SHELL=(/bin/bash -l)
 #
 #   git_dotfiles home "$HOME"
 #   git_dotfiles system /
-git_dotfiles() { local NAME="${1:?}" WORKTREE="${2:?}"
-	[[ -n "$NAME" ]]     || return "$(_gdf_err "name not specified")"
+git_dotfiles() {
+	local NAME="${1:?}" WORKTREE="${2:?}"
+	[[ -n "$NAME" ]] || return "$(_gdf_err "name not specified")"
 	[[ -n "$WORKTREE" ]] || return "$(_gdf_err "worktree not specified")"
 
 	local ALIAS="$NAME"
 	local CUSTOM_DOT_GIT="$WORKTREE/$NAME.git"
-	
+
 	# If this shell has been launched by the above alias...
 	if [[ "$GIT_DIR" = "$CUSTOM_DOT_GIT" ]]; then
 		_gdf_configure_shell "$NAME" "$WORKTREE"
 	else
 		# Create the alias to launch a new shell for interacting with this git repo.
-		# shellcheck disable=SC2139 # We want the alias definition to be expanded righ now.
+		# shellcheck disable=SC2139 # We want the alias definition to be expanded right now.
 		alias "$ALIAS=_gdf_launch_shell '$WORKTREE' '$CUSTOM_DOT_GIT'"
 	fi
 }
 
-_gdf_launch_shell() { local WORKTREE="${1:?}"; CUSTOM_DOT_GIT="${2:?}"
+_gdf_launch_shell() {
+	local WORKTREE="${1:?}"
+	CUSTOM_DOT_GIT="${2:?}"
 	[[ -d "$WORKTREE" ]] || return "$(_gdf_err "worktree '$WORKTREE' is not a directory")"
 	# If the worktree is not writable by the current user, we'll launch the shell with sudo...
 	local SUDO
@@ -45,7 +50,8 @@ _gdf_launch_shell() { local WORKTREE="${1:?}"; CUSTOM_DOT_GIT="${2:?}"
 
 # _gdf_init_repo ensures the worktree is set up as a git repo. It uses subshell semantics
 # so it doesn't affect the current shell session.
-_gdf_init_repo() ( local WORKTREE="${1:?}" CUSTOM_DOT_GIT="${2:?}"
+_gdf_init_repo() (
+	local WORKTREE="${1:?}" CUSTOM_DOT_GIT="${2:?}"
 	cd "$WORKTREE" || return "$(_gdf_err "failed to CD to worktree '$WORKTREE'")"
 	export GIT_DIR="$CUSTOM_DOT_GIT"
 	[[ -d "$CUSTOM_DOT_GIT" ]] || {
@@ -53,10 +59,12 @@ _gdf_init_repo() ( local WORKTREE="${1:?}" CUSTOM_DOT_GIT="${2:?}"
 		git init || return "$(_gdf_err "failed to initialize git repo")"
 	}
 	git config status.showUntrackedFiles no || return "$(_gdf_err "failed to configure git")"
+	git config core.excludesFile
 )
 
 # _gdf_configure_shell is called inside the login shell launched by the alias.
-_gdf_configure_shell() { local NAME="${1:?}" WORKTREE="${2:?}"
+_gdf_configure_shell() {
+	local NAME="${1:?}" WORKTREE="${2:?}"
 	cd "$WORKTREE" || return "$(_gdf_err "failed to CD to worktree '$WORKTREE'")"
 	export PS1="$NAME.git> $PS1"
 	if [[ $(type -t git_dotfiles_configure_shell_hook) == function ]]; then
