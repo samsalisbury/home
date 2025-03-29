58f1969106207de4b47f462e282c6daf935d8239
Sam Salisbury <samsalisbury@gmail.com>
2025-03-29 12:53:02 +0000

    vim: swift + xcodebuild support

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -14,6 +14,7 @@ return {
         servers = {
           ltex = false,
           ltex_ls = false,
+          sourcekit = true,
         },
         diagnostics = {
           virtual_text = false,
