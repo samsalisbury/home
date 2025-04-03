1501e69f36b14a64e52a9df92998923bc849135f
Sam Salisbury <samsalisbury@gmail.com>
2025-04-03 05:13:01 +0100

    nvim/tmux nagivation

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,10 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "f", "za", { desc = "Toggle fold under cursor" })
+
+vim.keymap.set("n", "<C-h>", ":NvimTmuxNavigateLeft<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", ":NvimTmuxNavigateDown<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", ":NvimTmuxNavigateUp<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", ":NvimTmuxNavigateRight<CR>", { silent = true })
+vim.keymap.set("n", "<C-\\>", ":NvimTmuxNavigateLastActive<CR>", { silent = true })
+vim.keymap.set("n", "<C-Space>", ":NvimTmuxNavigateNext<CR>", { silent = true })
