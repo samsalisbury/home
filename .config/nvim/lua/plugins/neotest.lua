55b35193f32f6bdd54c1f8ba6c75c3f1067e71e3
Sam Salisbury <samsalisbury@gmail.com>
2025-04-04 16:04:05 +0100

    nvim: remove edgy; better neotest icons

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-neotest/neotest",
+  icons = {
+    expanded = "",
+    child_prefix = "",
+    child_indent = "",
+    final_child_prefix = "",
+    non_collapsible = "",
+    collapsed = "",
+
+    passed = "",
+    running = "",
+    failed = "",
+    unknown = "",
+  },
+}
