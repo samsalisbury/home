b9081cfcb9d8d8af101ddcd6171491d67737b0e0
Sam Salisbury <samsalisbury@gmail.com>
2025-04-04 16:45:05 +0100

    disable nvim-cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    opts = {
-      -- Slow down completion suggestions
-      performance = {
-        debounce = 250,
-        throttle = 1000,
-      },
-    },
-  },
+  --{
+  --  "hrsh7th/nvim-cmp",
+  --  opts = {
+  --    -- Slow down completion suggestions
+  --    performance = {
+  --      debounce = 250,
+  --      throttle = 1000,
+  --    },
+  --  },
+  --},
 }
