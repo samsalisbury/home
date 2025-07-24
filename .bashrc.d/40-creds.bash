3bee0bf1356a03a530afd305fef061db4ee8f955
Sam Salisbury <samsalisbury@gmail.com>
2025-07-24 15:58:31 +0100

    improve auto gh credential loading

--- .bashrc.d/40-creds.bash
+++ .bashrc.d/40-creds.bash
@@ -4,30 +4,37 @@ creds() {
 
 	# This function redefines itself on first run.
 	creds() {
-		test -n "$1" || { echo "usage: $0 <name>"; return 1; }
+		test -n "$1" || {
+			echo "usage: $0 <name>"
+			return 1
+		}
 		local FILE="$HOME/.creds/$1"
-		test -e "$FILE" || { echo "no creds matching $1"; return 1; }
+		test -e "$FILE" || {
+			echo "no creds matching $1"
+			return 1
+		}
 		# shellcheck disable=SC1090 # Purposefully variable source.
 		source "$FILE"
 		echo "$1 credentials loaded"
 	}
 
 	# GitHub - auto-load credentials if available.
-	gh() {
+	gh() (
 		local BIN
-		BIN="$(which gh)"
-		(
-			creds github > /dev/null 2>&1
-			"$BIN" "$@"
-		)
-	}
-	
+		BIN="$(which gh)" || {
+			log "ERROR: gh binary not found in path"
+			return 1
+		}
+		{ [[ -z "$GH_TOKEN" ]] && creds github; } || log "Using GH_TOKEN from environment"
+		"$BIN" "$@"
+	)
+
 	# Git - auto-load github credentials if available.
 	git() {
 		local BIN
 		BIN="$(which git)"
 		(
-			creds github > /dev/null 2>&1
+			creds github >/dev/null 2>&1
 			"$BIN" "$@"
 		)
 	}
