d58d4bd967a82d89d341bbe2eae4c0912802df21
Sam Salisbury <samsalisbury@gmail.com>
2026-09-03 12:42:25 +0100

    try to make markdownlint respect config

---
+++ .config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,12 @@
+local HOME = os.getenv("HOME")
+return {
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = { "--config", HOME .. ".config/markdownlint-cli2.yaml", "--" },
+      },
+    },
+  },
+}
